2068e30011abbd332db9ec458e07eeba89a87a92
Mario <mario.caballero.948@gmail.com>
2022-08-22 23:00:21 -0300

    Initial files

---
+++ src/db.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pfdatabase`,
+  {
+    logging: false,
+    native: false,
+  }
+);
+
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
+
+modelDefiners.forEach((model) => model(sequelize));
+
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// const { Model1, Model2 } = sequelize.models;
+
+// Model1.belongsToMany(Model2, { through: "TabalIntermedia" });
+// Model2.belongsToMany(Model1, { through: "TabalIntermedia" });
+
+module.exports = {
+  ...sequelize.models,
+  conn: sequelize,
+};
