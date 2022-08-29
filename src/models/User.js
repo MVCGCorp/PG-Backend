95c46473b114ce1d1d1907e3baa32654de783a1e
federicofucci21 <federicofucci@hotmail.com>
2022-08-29 11:54:02 -0300

    mal escrito lastname

--- src/models/User.js
+++ src/models/User.js
@@ -12,7 +12,7 @@ module.exports = (sequelize) => {
             isAlpha: true
         }
       },
-      lasname: {
+      lastname: {
         type: DataTypes.STRING,
         allowNull: false,
       },
