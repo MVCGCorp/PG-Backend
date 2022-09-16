7cffff00f841833f0bc8a2696ef63447dc4b0f45
Mario <mario.caballero.948@gmail.com>
2022-09-16 08:13:39 -0300

    fix middleware

--- src/Middlewares/isAdmin.js
+++ src/Middlewares/isAdmin.js
@@ -1,11 +1,12 @@
-
-module.exports = (req, res, next) => {    
-    if (req.loginUser.rol === "admin" || req.loginUser.rol === "mododios") {
-		console.log('Bienvenido Administrador')
-				next();
-	} else {
-		console.log('Acceso no autorizado')
-		res.status(401).json({ msg: "Acceso no autorizado" })
-	}
-}
-
+module.exports = (req, res, next) => {
+  if (
+    req.body.loginUser.rol === "admin" ||
+    req.body.loginUser.rol === "mododios"
+  ) {
+    console.log("Bienvenido Administrador");
+    next();
+  } else {
+    console.log("Acceso no autorizado");
+    res.status(401).json({ msg: "Acceso no autorizado" });
+  }
+};
