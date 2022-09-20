86ce9b76df3258b1032354a29140e689467a2056
hidalgo86 <95661987+hidalgo86@users.noreply.github.com>
2022-09-19 23:49:24 -0300

    prueba

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const route = express.Router();
-const {User, Order, OrderDetail } = require("../db.js");
+const { Order, OrderDetail } = require("../db.js");
 const { STRIPE } = process.env;
 const stripe = require("stripe")(STRIPE);
 
@@ -29,7 +29,7 @@ route.post("/create-payment-intent", async (req, res) => {
       status: "procesando",
     });
 
-    await newOrder.addUser(userId);
+    await newOrder.setUser(userId);
 
     orderDetail = await OrderDetail.create({
       price,
